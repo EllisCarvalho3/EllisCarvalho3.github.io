72d2d39c06b2c3690639809a03967c44b16093fa
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2025-04-07 14:01:07 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const galleryConatiner = document.querySelector('.gallery-conatiner');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+    constructor(container, controls, items) {
+        this.carouselContainer = container;
+        this.carouselCon
+    }
+}
